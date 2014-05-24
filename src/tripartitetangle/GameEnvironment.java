60533d582184e3b7f9b8d260dd68a50a652f52e8
ONLYUSEtheSPOONS <doughill@lenovo-g530.brentwood.bc.ca>
2014-05-24 11:27:26 -0600

    new changes

--- src/tripartitetangle/GameEnvironment.java
+++ src/tripartitetangle/GameEnvironment.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package tripartitetangle;
 
 import environment.Environment;
@@ -23,32 +22,47 @@ class GameEnvironment extends Environment {
 
     Student student;
     ArrayList<PaintPalette> paintPalettes;
-    
+
     @Override
     public void initializeEnvironment() {
         this.setBackground(ResourceTools.loadImageFromResource("resources/school_field.jpg"));
-        
-        student = new Student(new Point(100, 400), new Velocity(0,0));
+
+        student = new Student(new Point(100, 400), new Velocity(0, 0));
         getActors().add(student);
-        
+
         paintPalettes = new ArrayList<PaintPalette>();
         for (int i = 0; i < 10; i++) {
             PaintPalette pp = new PaintPalette(new Point(150 + (i * 50), 450 - (i * 10)), new Velocity(0, 0));
             paintPalettes.add(pp);
             getActors().add(pp);
         }
+
+        PaintPalette pp = new PaintPalette(new Point(150 + (int) (Math.random() * 400), -200), new Velocity(0, 3));
+        paintPalettes.add(pp);
+        getActors().add(pp);
+
+        pp = new PaintPalette(new Point(150 + (int) (Math.random() * 400), -400), new Velocity(0, 1));
+        paintPalettes.add(pp);
+        getActors().add(pp);
+
+        pp = new PaintPalette(new Point(150 + (int) (Math.random() * 400), -100), new Velocity(0, 5));
+        paintPalettes.add(pp);
+        getActors().add(pp);
+
+    }
+
+    
+    
+    @Override
+    public void timerTaskHandler() {
+        checkCollection();
         
         
     }
 
-    @Override
-    public void timerTaskHandler() {
-        checkCollection();
-    }
-    
-    private void checkCollection(){
+    private void checkCollection() {
         for (PaintPalette pp : paintPalettes) {
-            if (pp.intersects(student)){
+            if (pp.intersects(student)) {
                 pp.setPosition(-100, -100);
                 //add to the score?  make a sound?
             }
@@ -58,9 +72,9 @@ class GameEnvironment extends Environment {
     @Override
     public void keyPressedHandler(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-            student.getVelocity().x = -2;
+            student.getVelocity().x = -5;
         } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            student.getVelocity().x = +2;
+            student.getVelocity().x = +5;
         } //else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
 //            student.getVelocity().x = +2;
 //        }
@@ -69,7 +83,7 @@ class GameEnvironment extends Environment {
 
     @Override
     public void keyReleasedHandler(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+        if ((e.getKeyCode() == KeyEvent.VK_RIGHT) || (e.getKeyCode() == KeyEvent.VK_LEFT)) {
             student.stop();
         }
     }
@@ -84,6 +98,4 @@ class GameEnvironment extends Environment {
 
     }
 
-
-    
 }
